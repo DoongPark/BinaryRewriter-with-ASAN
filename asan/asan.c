ba365fbaa1c9aa6ef8b77eb5301856d39093b6ab
doongpark <dooongpark@gmail.com>
2024-01-02 02:14:57 +0900

    feat: update asan features

--- asan/asan.c
+++ asan/asan.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <signal.h>
 
 #define REDZONE_SIZE 32
 #define REDZONE_PATTERN 0xfb
-
-unsigned long OFFSET = 0;
-unsigned int *shadow;
+#define OFFSET 0x20000000
+#define SHADOW_MEM_SIZE 1024*1024*10
+#define SEGVALUE 0x20000000
 
 void* heap_to_shadow(void* addr) {
-    return ((unsigned long)addr >> 3) + OFFSET;
+    return ((unsigned long)addr >> 3) + (*(unsigned long*) OFFSET);
 }
 
 void* asan_malloc(unsigned int n) {
@@ -33,25 +34,125 @@ void* asan_malloc(unsigned int n) {
     }
 
     // 실제 사용자에게 반환할 메모리 주소
-    return mem + REDZONE_SIZE;
+    return mem + REDZONE_SIZE - SEGVALUE;
 }
 
-void asan_check(void* addr, int offset) {
-    unsigned char *shadow_addr = (unsigned char*)heap_to_shadow(addr + offset); 
-    if (*shadow_addr == 0xff || (*shadow_addr != 0x00 && *shadow_addr <= offset % 8)) {
-        printf("asan: heap overflow\n");
+void asan_free(void* addr) {
+    // 메모리 주소에 해당하는 shadow 메모리를 0xfb로 초기화
+    memset(heap_to_shadow(addr - REDZONE_SIZE), 0xfb, (REDZONE_SIZE + sizeof(addr) + REDZONE_SIZE) >> 3);
+    // 실제 메모리 해제
+    free(addr - REDZONE_SIZE);
+}
+
+void asan_check(void* addr) {
+    unsigned char *shadow_addr = (unsigned char*)heap_to_shadow(addr);
+
+    if (*shadow_addr == 0xfb) {
+        perror("asan: use-after-free\n");
+        asm(
+            /* exit */
+            "mvn r0, #0\n" /* exit status */
+            "mov r7, #1\n" /* syscall id */
+            "swi #0"
+        );
+    } else if (*shadow_addr == 0xff) {
+        // fprintf(stderr, "asan: heap overflow (0x%x)\n", *shadow_addr);
+        perror("asan: heap overflow\n");
+        asm(
+            /* exit */
+            "mvn r0, #0\n" /* exit status */
+            "mov r7, #1\n" /* syscall id */
+            "swi #0"
+        );
     }
 }
+
+void* asan_calloc(int n, int size) {
+    return asan_malloc(n * size);
+}
+
+void* asan_realloc(void* ptr, int size) {
+    asan_free(ptr);
+    return asan_malloc(size);
+}
+
+int is_asan_target(void* addr) {
+  return (SEGVALUE & (unsigned int)addr) == SEGVALUE;
+}
+
+/* return the closest element's index */
+int closest(unsigned long* arr, int len, unsigned long n)
+{
+    unsigned long diff = __INT_MAX__;
+    int ret = -1;
+    for (int i = 0; i < len; i++) {
+    int cur_diff = arr[i] - n;
+    if (cur_diff < diff) {
+        ret = i;
+        diff = cur_diff;
+    }
+    }
+    return ret;
+}
+
+void handler(int nSignum, siginfo_t* si, void* vcontext) {
+    if (!is_asan_target((void*)si->si_addr + SEGVALUE)) {
+    /* not sanitizable */
+    printf("Segmentation fault at %p\n", si->si_addr);
+    exit(-1);
+    }
+    asan_check((void*)si->si_addr + SEGVALUE);
+
+    /* Valid access. Restore the address */
+    ucontext_t* context = (ucontext_t*)vcontext;
+    unsigned long reg[8] = {
+    context->uc_mcontext.arm_r0,
+    context->uc_mcontext.arm_r1,
+    context->uc_mcontext.arm_r2,
+    context->uc_mcontext.arm_r3,
+    context->uc_mcontext.arm_r4,
+    context->uc_mcontext.arm_r5,
+    context->uc_mcontext.arm_r6,
+    context->uc_mcontext.arm_r7
+    };
+    unsigned long* p_reg[8] = {
+    &context->uc_mcontext.arm_r0,
+    &context->uc_mcontext.arm_r1,
+    &context->uc_mcontext.arm_r2,
+    &context->uc_mcontext.arm_r3,
+    &context->uc_mcontext.arm_r4,
+    &context->uc_mcontext.arm_r5,
+    &context->uc_mcontext.arm_r6,
+    &context->uc_mcontext.arm_r7
+    };
+
+    printf("Restore addr %p\n", si->si_addr);
+    for (int i = 0; i < 8; i++) {
+    printf("r%d: 0x%x\n", i, reg[i]);
+    }
+    /* 하위 3개비트 = [Rn] */
+    int target_reg = *(unsigned long*)(void*)context->uc_mcontext.arm_pc & 0b111;
+    /* Assumes the closest address is trying to access */
+    // int target_reg = closest(reg, 8, si->si_addr);
+    *p_reg[target_reg] += SEGVALUE;
+    // context->uc_mcontext.arm_pc++;
+}
+
+void asan_init() {
+    memset(OFFSET, 0, SHADOW_MEM_SIZE);
+    struct sigaction action;
+    memset(&action, 0, sizeof(struct sigaction));
+    action.sa_flags = SA_SIGINFO;
+    action.sa_sigaction = handler;
+    sigaction(SIGSEGV, &action, NULL);
+}
+
 
 int main() {
-    shadow = (unsigned int*)malloc(1024);
-    OFFSET = (unsigned long)(shadow) - ((unsigned long)(shadow + 1024) >> 3);
+    asan_init();
 
-    char* a = (char*)asan_malloc(10);
-    for (int i = 0; i < 14; i++) {
-        asan_check(a, i);
-        printf("access a[%d]\n", i);
-        a[i] = 42; // heap overflow
-    }
+    int* x = asan_malloc(10);
+    int y = *x;
+
     return 0;
 }
